5a9dda398b5dc096fc1f05ed88ae7b8ddd69a712
memekomik <memekomm66@gmail.com>
2022-09-22 11:49:58 +0700

    Update cb.cpp

--- cabang/cb.cpp
+++ cabang/cb.cpp
@@ -2,5 +2,29 @@
 
 using namespace std;
 
-int main (){
-    
+int main(){
+	
+	int suhu;
+	cout<<"Program menentukan wujud air"<<endl;
+	cout<<"\nMasukkan suhu:";
+	cin>>suhu;
+	
+	if (suhu<=0){
+		
+		cout<<"\nAir berwujud beku";
+		
+	}
+	
+	else if (suhu>0&&suhu<=100){
+		
+		cout<<"\nAir berwujud cair";
+		
+	}
+	
+	else {
+		
+		cout<<"\nAir berwujud uap";
+	}
+	
+	return 0;
+}
