8cff9fd428436c9cf88cce073717e807da29c147
memekomik <memekomm66@gmail.com>
2022-11-03 10:44:31 +0700

    Create fungsinilai.cpp

---
+++ fungsi/fungsinilai.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int nilai;
+	string nilaiHuruf, rentangNilai;
+	
+	cout << "Masukkan nilai anda: ";
+	cin >> nilai;
+	
+	nilaiHuruf = rentangNilai;
+	cout << "Predikat anda adalah: " << nilaiHuruf << endl;
+	
+	cin.get()
+	return 0;
+
+}
+
+string rentangNilai (int x){
+	string y;
+	if (x >= 76) {
+		y = "A";
+		}
+	else if (x < 76 &&  x >= 71) {
+		y = "B+";
+		}
+	else if (x < 71 && x >= 66) {
+		y = "B";
+		}
+	else if (x < 66 && x >= 61) {
+		y = "C+";
+		}
+	else if (x < 61 && x >= 56) {
+		y = "C";
+		}
+	else if (x < 56 && x >= 51) {
+		y = "D";
+		}
+	else if (x < 51 && x >= 0) {
+		y = "E";
+		}
+	else {
+		y = "bukan merupakan nilai yang valid.";
+		}
+	return y;
+}
